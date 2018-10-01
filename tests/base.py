5728387622ce0b7f8b6d56d741c9a5f669aa85d6
litwachi <katuka.wilfred8@gmail.com>
2018-10-01 17:09:06 +0300

    User authetication tests

---
+++ tests/base.py
@@ -0,0 +1,26 @@
+import unittest
+from config import BaseConfig
+
+#app = create_app("config.BaseConfig")
+
+
+class BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.client = app.test_client()
+        self.data = {
+            'username': 'litwachi',
+            'email': 'katuka.wilfred8@gmail.com',
+            'password': 'password',
+            'database': BaseConfig.TEST_DB,
+            'test_logout_token': None
+        }
+        """ Login to get a JWT token """
+        self.client.post('/api/v1/auth/signup', json=self.data)
+        response = self.client.post('/api/v1/auth/login', json=self.data)
+        self.token = response.get_json().get('auth_token')
+        self.user_id = '1'
+
+    def tearDown(self):
+        # method to invoke after each test.
+        pass
